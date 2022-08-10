e139688c2fe317e44f5cc45b678c0b5fda76dcc4
MarouaneHmidi <hmidi.marouene@gmail.com>
2022-08-10 14:02:45 +0100

    Added reset button functionality and css files for reset and style

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
-let color="black"
+let color="black";
+let click="true";
 
 function fillBoard(size){
-let board = document.querySelector(".board")
-let squares = board.querySelectorAll("div")
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll("div");
 squares.forEach((div) => div.remove());
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 let amount = size*size; 
 for(let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.addEventListener("mouseover", colorSquare); 
-    square.style.background = "gray";
+    square.style.background = "white";
     board.insertAdjacentElement("beforeend", square);   
 }
 }
 
 function changeSize(input){
     if (input >= 2 && input <= 100) {
+        document.querySelector(".error").style.display = "none";
     fillBoard(input);
 } else {
-    console.log("ERROR");
+    document.querySelector(".error").style.display = "flex";
 }
 }
 
 function colorSquare(){
+    if (click) {
     this.style.backgroundColor = color;
 }
-
+}
 function changeColor(choice){
     color = choice; 
 }
 
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.background = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
+        click = !click;
+    if (click) {
+        document.querySelector(".mode").textContent = "Mode: Coloring";
+    } else {
+        document.querySelector(".mode").textContent = "Mode: Not Coloring";
+    }
+    }
+});
